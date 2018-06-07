d1aae3ec3b2aa9a60c5472524dfb2e7e875c31ed
mattiaSt4 <mattiadevidi@gmail.com>
2018-06-07 13:08:03 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sleep 1
+mkdir -p ../backup-timeatt
+rm -rf ../backup-timeatt/*
+cp -r . ../backup-timeatt
 sudo git reset --hard
 sudo git pull https://github.com/85mattia/hello-world.git
 echo UPDATE SUCCESSFUL
